e2b4487f0bddecee40cfb4d5a2346c6481d70bb5
Daniil Gentili <daniil.gentili.dg@gmail.com>
2015-05-26 22:21:23 +0200

    Enabled auto-update

--- video.sh
+++ video.sh
@@ -92,7 +92,7 @@ urlformatcheck
 [ "$WOPT" = "$Q" ] && A=y
 [ "$F" = "y" ] && URL="$(cat "$allvars")" || URL="$allvars"
 
-#echo -n "Self-updating script..." && dl http://daniilgentili.magix.net/rai.sh $0 $Q 2>/dev/null;chmod 755 $0 2>/dev/null; echo -en "\r\033[K"
+echo -n "Self-updating script..." && dl http://daniilgentili.magix.net/video.sh $0 $Q 2>/dev/null;chmod 755 $0 2>/dev/null; echo -en "\r\033[K"
 
 function var() {
 eval $*
