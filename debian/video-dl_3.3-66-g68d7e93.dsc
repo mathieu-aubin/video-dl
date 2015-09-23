9f285a03a3068353f0e29b969f976d429ac48777
Daniil <daniil.gentili.dg@gmail.com>
2015-09-23 19:16:34 +0000

    OK madebytravisci

---
+++ debian/video-dl_3.3-66-g68d7e93.dsc
@@ -0,0 +1,17 @@
+Format: 3.0 (native)
+Source: video-dl
+Binary: video-dl
+Architecture: all
+Version: 3.3-66-g68d7e93
+Maintainer: Daniil Gentili <daniil.gentili.dg@gmail.com>
+Homepage: http://daniil.it/video-dl
+Standards-Version: 3.9.4
+Build-Depends: debhelper (>= 8.0.0)
+Package-List: 
+ video-dl deb misc optional
+Checksums-Sha1: 
+ 10b58171dec99511d08b71d6888167129150a8bc 12757 video-dl_3.3-66-g68d7e93.tar.gz
+Checksums-Sha256: 
+ 5f61798105e0e2f0b7ab0b4a6c7052011424dbbfbe15ddee6bf99269541feb62 12757 video-dl_3.3-66-g68d7e93.tar.gz
+Files: 
+ ab1fb357270b6dda8aace15e4228d465 12757 video-dl_3.3-66-g68d7e93.tar.gz
--- video.sh
+++ video.sh
@@ -125,6 +125,7 @@ dl="$(echo $1 | grep -q '^//' && echo http:$1 || echo $1)"
 
 #dl="$(echo "$dl" | sed 's/#.*//;s/https:\/\//http:\/\//g')"
 
+
 urltype="$(curl -w "%{url_effective}\n" -L -s -I -S "$dl" -o /dev/null)"
 
 echo "$urltype" | grep -qE 'http://www.*.rai..*/dl/RaiTV/programmi/media/.*|http://www.*.rai..*/dl/RaiTV/tematiche/*|http://www.*.rai..*/dl/.*PublishingBlock-.*|http://www.*.rai..*/dl/replaytv/replaytv.html.*|http://.*.rai.it/.*|http://www.rainews.it/dl/rainews/.*|http://mediapolisvod.rai.it/.*|http://*.akamaihd.net/*|http://www.video.mediaset.it/video/.*|http://www.video.mediaset.it/player/playerIFrame.*|http://.*wittytv.it/.*|http://la7.it/.*|http://.*.la7.it/.*|http://la7.tv/.*|http://.*.la7.tv/.*' || ptype=common
@@ -673,8 +674,3 @@ done
 
 
 [ "$queue" = "" ] && { echo "ERROR: download list is empty."; exit 1; }
-[ "$play" != "y" ] && { echo "Downloading videos..." && eval "$queue" && echo "All downloads completed successfully." || echo "An error occurred";exit 1; } 
-[ "$play" = "y" ] && { eval $queue || echo "An error occurred";exit 1; }
-
-
-exit $?
