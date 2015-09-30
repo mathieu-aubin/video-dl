2e887dc6ce530f9b5c965d4d70a89421fa2f8326
Daniil <daniil.gentili.dg@gmail.com>
2015-09-30 18:12:18 +0000

    OK madebytravisci

--- debian/video-dl-3.3.96/video-dl/video.sh
+++ debian/video-dl-3.3.96/video-dl/video.sh
@@ -526,64 +526,6 @@ formatoutput
 
 ###########################################################################################
 ##################### End of Mediaset section, beginning of common section ################
-common() {
-# Store the page in a variable
-page="$(wget -q -O - $1)"
-# Get the video URLs
-base="$(echo "$page" | egrep '\.mp4|\.mkv|\.flv|\.f4v|\.wmv|\.mov|\.3gp|\.avi|\.m4v|\.mpg|\.mpe|\.mpeg' | sed 's/.*http:\/\//http:\/\//;s/\".*//' | sed "s/'.*//" | sed 's/.mp4.*/.mp4/g;s/.mkv.*/.mkv/g;s/.flv.*/.flv/g;s/.f4v.*/.f4v/g;s/.wmv.*/.wmv/g;s/.mov.*/.mov/g;s/.3gp.*/.3gp/g;s/.avi.*/.avi/g;s/.m4v.*/.m4v/g;s/.mpg.*/.mpg/g;s/.mpe.*/.mpe/g;s/.mpeg.*/.mpeg/g' | awk '!x[$0]++')"
-
-checkurl
-[ "$base" = "" ] && continue 
-
-videoTitolo="$(echo $page | sed 's/.*<title>//;s/<\/title>.*//' | sed 's/^ //')"
-
-title="${videoTitolo//[^a-zA-Z0-9 ]/}"
-
-title=`echo $title | tr -s " "`
-
-title=${title// /_}
-
-unformatted="$base"
-formatoutput
-
-}
-
-# OK, here's the problem: The following function uses youtube-dl to get the download info, but offering the functionality of youtube-dl using an http api is one thing, while using the above mentioned program when the user can already use it is another thing. Comment the auto update line and change common_yt_dl to common if you still want to use youtube-dl trough this script.
-
-common_yt_dl() {
-tmpjson="$(youtube-dl -J "$1")"
-videoTitolo=$(echo "$tmpjson" | sed 's/.*\"title\": \"//g;s/\".*//g')
-json="$(echo "$tmpjson" | sed 's/.*formats//g;s/, [{]/\
-/g')"
-while read -r line; do
-    l=$(echo "$line" | sed 's/,/\
-/g')
-    for f in format url ext; do
-     temp="$(echo "$l" | grep \"$f\": | sed 's/"'$f'"\: "//g;s/"$//g;s/^ //g;s/^.* - //g')"
-     eval $f=\""$temp"\"
-    done
-    [ "$url" != "" ] && {
-size=
-timeout -skill 3s wget -S --spider "$url" &>/dev/null && size=", $(wget -S --spider "$url" 2>&1 | grep -E '^Length|^Lunghezza' | sed 's/.*(//;s/).*//')B" || size=", Unkown size"
-format=$(echo "$format" | sed 's/[(]//g;s/[)]//g')
-formats="$formats
-$format ($ext$size) $url"
-    }
-done <<< "$json"
-
-# Get the title
-title="${videoTitolo//[^a-zA-Z0-9 ]/}"
-title=`echo $title | tr -s " "`
-title=${title// /_}
-
-}
-
-###########################################################################################
-##################### End of Common section, beginning of database section ################
-$ptype $dl $2 $3
-[ "$formats" = "" ] && continue || echo "$title $videoTitolo
-$formats"
-}
 # 2nd part starts here
 }
 }
---
+++ debian/video-dl_3.3.96.dsc
@@ -0,0 +1,38 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA1
+
+Format: 3.0 (native)
+Source: video-dl
+Binary: video-dl
+Architecture: all
+Version: 3.3.96
+Maintainer: Daniil Gentili <daniil.gentili.dg@gmail.com>
+Homepage: http://daniil.it/video-dl
+Standards-Version: 3.9.4
+Build-Depends: debhelper (>= 8.0.0)
+Package-List: 
+ video-dl deb misc optional
+Checksums-Sha1: 
+ 38fd702987965bec4ded377ec832f0144738e6fe 12794 video-dl_3.3.96.tar.gz
+Checksums-Sha256: 
+ 6f16cb16edb29cca421aea718a24e770eaa1ee4bfb780518d3731cebd511835b 12794 video-dl_3.3.96.tar.gz
+Files: 
+ 7478ae0b68603d61c0d436afe7a8cc79 12794 video-dl_3.3.96.tar.gz
+
+-----BEGIN PGP SIGNATURE-----
+Version: GnuPG v1.4.11 (GNU/Linux)
+
+iQIcBAEBAgAGBQJWDCX9AAoJEHK5f9HZZyyTI8wP/1tsKh2q7iGwWUcVyYDtgEcy
+QvjMKKdtuXZEchkIgOrhteMlnFnh6mOWsvdAe/KvLi5WhBB5wl2fLqdrjaqJtuOw
+IkQh/u0nbOh8+iXet1UY3gYpYmZWbRliJICjYd5NFGIpS1Iqe9O1Af18d9g9sfJc
+msuhkVmzd8xq60fVROKrd2sThMsFf+dyRm+bo/fAp/Fdi98jD9F4fFNcGyNSw9pd
+HxncDM3KNdoXCn9jmAoahcl591PWnd1EMEMboA8p+ASW9PMsOxbWa4GzswMvp7C0
+gFuZpAsSuLoY/wEQ6Xsy9uz1XHjbccNbiio4j769lWarkamIhzQ4VSDRbojb+bwu
+SdJ/Esu0LrOL7mrsKAq3IAtOeZvX9VLqFiQQ8A84hyWP5xKcDxLIB5foH46HFiVy
+fB6Lah3+IOn22naDoz0jKjh3eWQmZrIpm/g9HcqigwYnp3kWMYQm/1Rc5vBZXenS
+YWOjtgkGPHX9fOKi0PGs1BS6awlIZOXYozeA4Hq0SKIog2prk3Yo8ZaA2Yepn53c
+F2jMSQzZ7wJjek2QcckRDKD5cm3AhJp9P4Flcq5FHptXhtC51AzKwS4QjaJ15Ey5
+wnDuF7HbWiJS0FZr6Xq5w5G4Fx70qtSRRgYvFLJ8GERwX0x8eJTYPkh7WWtFzVt2
+0PcyHH/2nMKgODgEOw5w
+=+Hfx
+-----END PGP SIGNATURE-----
