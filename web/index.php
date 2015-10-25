017e855f944626d92e8ac211665356da297ceafd
Daniil Gentili <daniil.gentili.dg@gmail.com>
2015-10-25 17:42:40 +0100

    Database, API and web version sync

--- web/index.php
+++ web/index.php
@@ -374,7 +374,7 @@ Thanks!</textarea>
     <script src="https://cdn.rawgit.com/mathiasbynens/he/master/he.js"></script>
     <script src="/js/linkify.min.js?"></script>
     <script src="/js/linkify-jquery.min.js?"></script>
-    <script data-cfasync="false" src="/js/video-dl.js?v=tynixpowerlucedeidiamantimagicwinxminimondimagiciwinx"></script>
+    <script data-cfasync="false" src="/video-dl-jquery/video-dl.js?v=tynixpowerlucedeidiamantimagicwinxminimondimagiciwinx"></script>
 
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
